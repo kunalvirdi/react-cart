2bec38f7c358a3130bd308c0a44cd2d349123a5d
kunalvirdi <kunalvirdi.patiala@gmail.com>
2024-02-14 10:33:13 +0530

    done redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import {Provider} from "react-redux";
-import {store} from "../store/index.js";
 
 const App=()=>{
   return (
-    <Provider store={store}>
+    <>
         <h1>Hello React+Vite</h1>
-    </Provider>
+    </>
   )
 }
 
